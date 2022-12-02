fb5f02adfe5feb53af4f2da2fa513b7d9b761f08
Suzy Gebbett <suzgebbett@gmail.com>
2022-12-02 10:30:44 +0000

    Day 1 completed!

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,4 +1,14 @@
 module Day1 where
+import qualified Data.Text as T
+import Data.List.Split as D
+import Data.List as L
 
-doAThing :: Int -> Int
-doAThing x = 2*x
+main :: IO ()
+main = do 
+      contents <- readFile "day1.txt"
+      let elves = D.splitOn "\n\n" contents
+      let elves2 = map (D.splitOn "\n") elves
+      let elves3 = map (map (read)) elves2
+      let elves4 = (L.sort (map sum elves3))
+      let elves5 = last (elves4) + head(drop 1 (reverse elves4)) + head(drop 2 (reverse elves4))
+      print elves5
